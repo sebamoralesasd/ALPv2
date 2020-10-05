3e9b836363125881edab08613fc249924963d19e
SebastianMoline <sebastianmoline@gmail.com>
2020-10-05 14:39:31 -0300

    Eval3 finalizado. Corrección de Eval1 y Eval2 para mejor lectura

--- TP1/code/src/Eval1.hs
+++ TP1/code/src/Eval1.hs
@@ -18,8 +18,8 @@ initState = M.empty
 -- Busca el valor de una variable en un estado
 lookfor :: Variable -> State -> Int
 lookfor v s  = case M.lookup v s of
-                    Just x -> x
-                    Nothing -> error "Variable no definida dentro del entorno."
+               Just x -> x
+               Nothing -> error "Variable no definida dentro del entorno."
 
 -- Cambia el valor de una variable en un estado
 update :: Variable -> Int -> State -> State
@@ -37,60 +37,60 @@ stepCommStar c    s = Data.Strict.Tuple.uncurry stepCommStar $ stepComm c s
 
 -- Evalua un paso de un comando en un estado dado
 stepComm :: Comm -> State -> Pair Comm State
-stepComm (Let var intE) state = let (i :!: newState) = evalExp intE state
-                                in (Skip :!: (update var i newState))
-stepComm (Seq comm1 comm2) state = case (stepComm comm1 state) of
-                                        (Skip :!: newState) -> (comm2 :!: newState)
-                                        (c :!: newState) -> ((Seq c comm2) :!: newState)
+stepComm (Let var intE)                 state = let (i :!: newState) = evalExp intE state
+                                                in (Skip :!: (update var i newState))
+stepComm (Seq comm1 comm2)              state = case (stepComm comm1 state) of
+                                                (Skip :!: newState) -> (comm2 :!: newState)
+                                                (c :!: newState) -> ((Seq c comm2) :!: newState)
 stepComm (IfThenElse boolE comm1 comm2) state = case (evalExp boolE state) of
-                                                     (True :!: newState) -> (comm1 :!: newState)
-                                                     (False :!: newState) -> (comm2 :!: newState)
-stepComm (While boolE comm) state = case (evalExp boolE state) of
-                                         (True :!: newState) -> ((Seq comm (While boolE comm)) :!: newState)
-                                         (False :!: newState) -> (Skip :!: newState)
+                                                (True :!: newState) -> (comm1 :!: newState)
+                                                (False :!: newState) -> (comm2 :!: newState)
+stepComm (While boolE comm)             state = case (evalExp boolE state) of
+                                                (True :!: newState) -> ((Seq comm (While boolE comm)) :!: newState)
+                                                (False :!: newState) -> (Skip :!: newState)
 
 -- Evalua una expresion
 evalExp :: Exp a -> State -> Pair a State
 evalExp (Const int) state = (int :!: state)
-evalExp (Var variable) state = (lookfor variable state :!: state)
-evalExp (EAssgn var intE) state = let (i :!: newState) = evalExp intE state
-                                  in (i :!: update var i newState)
-evalExp (ESeq intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
-                                       (i2 :!: state2) = evalExp intE2 state1
-                                   in (i2 :!: state2)
-evalExp (UMinus intE) state = let (i :!: newState) = evalExp intE state
-                              in ((-i) :!: newState)
-evalExp (Plus intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
-                                       (i2 :!: state2) = evalExp intE2 state1
-                                   in ((i1 + i2) :!: state2)
+evalExp (Var variable)      state = (lookfor variable state :!: state)
+evalExp (EAssgn var intE)   state = let (i :!: newState) = evalExp intE state
+                                    in  (i :!: update var i newState)
+evalExp (ESeq intE1 intE2)  state = let (i1 :!: state1) = evalExp intE1 state
+                                        (i2 :!: state2) = evalExp intE2 state1
+                                     in (i2 :!: state2)
+evalExp (UMinus intE)       state = let (i :!: newState) = evalExp intE state
+                                    in  ((-i) :!: newState)
+evalExp (Plus intE1 intE2)  state = let (i1 :!: state1) = evalExp intE1 state
+                                        (i2 :!: state2) = evalExp intE2 state1
+                                    in  ((i1 + i2) :!: state2)
 evalExp (Minus intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
                                         (i2 :!: state2) = evalExp intE2 state1
-                                    in ((i1 - i2) :!: state2)
+                                    in  ((i1 - i2) :!: state2)
 evalExp (Times intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
                                         (i2 :!: state2) = evalExp intE2 state1
-                                    in ((i1 * i2) :!: state2)
-evalExp (Div intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
-                                      (i2 :!: state2) = evalExp intE2 state1
-                                  in ((div i1 i2) :!: state2)
-evalExp BTrue state = (True :!: state)
-evalExp BFalse state = (False :!: state)
-evalExp (Lt intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
-                                     (i2 :!: state2) = evalExp intE2 state1
-                                 in ((i1 < i2) :!: state2)
-evalExp (Gt intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
-                                     (i2 :!: state2) = evalExp intE2 state1
-                                 in ((i1 > i2) :!: state2)
+                                    in  ((i1 * i2) :!: state2)
+evalExp (Div intE1 intE2)   state = let (i1 :!: state1) = evalExp intE1 state
+                                        (i2 :!: state2) = evalExp intE2 state1
+                                    in  ((div i1 i2) :!: state2)
+evalExp BTrue               state = (True :!: state)
+evalExp BFalse              state = (False :!: state)
+evalExp (Lt intE1 intE2)    state = let (i1 :!: state1) = evalExp intE1 state
+                                        (i2 :!: state2) = evalExp intE2 state1
+                                    in  ((i1 < i2) :!: state2)
+evalExp (Gt intE1 intE2)    state = let (i1 :!: state1) = evalExp intE1 state
+                                        (i2 :!: state2) = evalExp intE2 state1
+                                    in  ((i1 > i2) :!: state2)
 evalExp (And boolE1 boolE2) state = let (b1 :!: state1) = evalExp boolE1 state
                                         (b2 :!: state2) = evalExp boolE2 state1
-                                    in ((b1 && b2) :!: state2)
-evalExp (Or boolE1 boolE2) state = let (b1 :!: state1) = evalExp boolE1 state
-                                       (b2 :!: state2) = evalExp boolE2 state1
-                                   in ((b1 || b2) :!: state2)
-evalExp (Not boolE) state = let (b :!: newState) = evalExp boolE state
-                            in ((not b) :!: newState)
-evalExp (Eq intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
-                                     (i2 :!: state2) = evalExp intE2 state1
-                                 in ((i1 == i2) :!: state2)
-evalExp (NEq intE1 intE2) state = let (i1 :!: state1) = evalExp intE1 state
-                                      (i2 :!: state2) = evalExp intE2 state1
-                                  in ((i1 /= i2) :!: state2)
+                                    in  ((b1 && b2) :!: state2)
+evalExp (Or boolE1 boolE2)  state = let (b1 :!: state1) = evalExp boolE1 state
+                                        (b2 :!: state2) = evalExp boolE2 state1
+                                    in  ((b1 || b2) :!: state2)
+evalExp (Not boolE)         state = let (b :!: newState) = evalExp boolE state
+                                    in  ((not b) :!: newState)
+evalExp (Eq intE1 intE2)    state = let (i1 :!: state1) = evalExp intE1 state
+                                        (i2 :!: state2) = evalExp intE2 state1
+                                    in  ((i1 == i2) :!: state2)
+evalExp (NEq intE1 intE2)   state = let (i1 :!: state1) = evalExp intE1 state
+                                        (i2 :!: state2) = evalExp intE2 state1
+                                    in  ((i1 /= i2) :!: state2)
--- TP1/code/src/Eval2.hs
+++ TP1/code/src/Eval2.hs
@@ -18,8 +18,8 @@ initState = M.empty
 -- Busca el valor de una variable en un estado
 lookfor :: Variable -> State -> Either Error Int
 lookfor v s  = case M.lookup v s of
-                    Just x -> Right x
-                    Nothing -> Left UndefVar
+               Just x -> Right x
+               Nothing -> Left UndefVar
 
 -- Cambia el valor de una variable en un estado
 update :: Variable -> Int -> State -> State
@@ -39,26 +39,26 @@ stepCommStar c    s = do
 
 -- Evalua un paso de un comando en un estado dado
 stepComm :: Comm -> State -> Either Error (Pair Comm State)
-stepComm (Let var intE) state = case evalExp intE state of
-                                     Left error -> Left error
-                                     Right (i :!: newState) -> Right (Skip :!: (update var i newState))
-stepComm (Seq comm1 comm2) state = case (stepComm comm1 state) of
-                                        Left error -> Left error
-                                        Right (Skip :!: newState) -> Right (comm2 :!: newState)
-                                        Right (c :!: newState) -> Right ((Seq c comm2) :!: newState)
+stepComm (Let var intE)                 state = case evalExp intE state of
+                                                Left error -> Left error
+                                                Right (i :!: newState) -> Right (Skip :!: (update var i newState))
+stepComm (Seq comm1 comm2)              state = case (stepComm comm1 state) of
+                                                Left error -> Left error
+                                                Right (Skip :!: newState) -> Right (comm2 :!: newState)
+                                                Right (c :!: newState) -> Right ((Seq c comm2) :!: newState)
 stepComm (IfThenElse boolE comm1 comm2) state = case (evalExp boolE state) of
-                                                     Left error -> Left error
-                                                     Right (True :!: newState) -> Right (comm1 :!: newState)
-                                                     Right (False :!: newState) -> Right (comm2 :!: newState)
-stepComm (While boolE comm) state = case (evalExp boolE state) of
-                                         Left error -> Left error
-                                         Right (True :!: newState) -> Right ((Seq comm (While boolE comm)) :!: newState)
-                                         Right (False :!: newState) -> Right (Skip :!: newState)
+                                                Left error -> Left error
+                                                Right (True :!: newState) -> Right (comm1 :!: newState)
+                                                Right (False :!: newState) -> Right (comm2 :!: newState)
+stepComm (While boolE comm)             state = case (evalExp boolE state) of
+                                                Left error -> Left error
+                                                Right (True :!: newState) -> Right ((Seq comm (While boolE comm)) :!: newState)
+                                                Right (False :!: newState) -> Right (Skip :!: newState)
 
 -- ~ Dado un posible par y una operacion unaria, devuelve un posible par
 -- ~ con la operación aplicada y el estado actualizado
 handleUnExpr :: Either Error (Pair a State) -> (a -> a) -> Either Error (Pair a State)
-handleUnExpr (Left error) _ = Left error
+handleUnExpr (Left error) _            = Left error
 handleUnExpr (Right (val :!: state)) f = Right (f val :!: state)
 
 -- ~ Dadas dos expreciones, el estado original y una operacion binaria,
@@ -68,35 +68,35 @@ handleBinExpr :: Exp a -> Exp a -> State -> (a -> a -> b) -> Either Error (Pair 
 handleBinExpr exp1 exp2 state f = case evalExp exp1 state of
                                   Left error -> Left error
                                   Right (i1 :!: state1) -> case evalExp exp2 state1 of
-                                                            Left error -> Left error
-                                                            Right (i2 :!: state2) -> Right (f i1 i2 :!: state2)
+                                                           Left error -> Left error
+                                                           Right (i2 :!: state2) -> Right (f i1 i2 :!: state2)
 
 -- Evalua una expresion
 evalExp :: Exp a -> State -> Either Error (Pair a State)
 evalExp (Const int) state = Right (int :!: state)
-evalExp (Var variable) state = case lookfor variable state of 
-                               Left error -> Left error
-                               Right i -> Right (i :!: state)
-evalExp (EAssgn var intE) state = case evalExp intE state of
-                                       Left error -> Left error
-                                       Right (i :!: newState) -> Right (i :!: update var i newState)
-evalExp (ESeq intE1 intE2) state = handleBinExpr intE1 intE2 state (\a b -> b)
-evalExp (UMinus intE) state = handleUnExpr (evalExp intE state) (\x -> (-x))
-evalExp (Plus intE1 intE2) state = handleBinExpr intE1 intE2 state (\a b -> a + b)
+evalExp (Var variable)      state = case lookfor variable state of 
+                                    Left error -> Left error
+                                    Right i -> Right (i :!: state)
+evalExp (EAssgn var intE)   state = case evalExp intE state of
+                                    Left error -> Left error
+                                    Right (i :!: newState) -> Right (i :!: update var i newState)
+evalExp (ESeq intE1 intE2)  state = handleBinExpr intE1 intE2 state (\a b -> b)
+evalExp (UMinus intE)       state = handleUnExpr (evalExp intE state) (\x -> (-x))
+evalExp (Plus intE1 intE2)  state = handleBinExpr intE1 intE2 state (\a b -> a + b)
 evalExp (Minus intE1 intE2) state = handleBinExpr intE1 intE2 state (\a b -> a - b)
 evalExp (Times intE1 intE2) state = handleBinExpr intE1 intE2 state (\a b -> a * b)
-evalExp (Div intE1 intE2) state = case evalExp intE1 state of
-                                   Left error -> Left error
-                                   Right (i1 :!: state1) -> case evalExp intE2 state1 of
-                                                            Left error -> Left error
-                                                            Right (0 :!: state2) -> Left DivByZero
-                                                            Right (i2 :!: state2) -> Right ((div i1 i2) :!: state2)
-evalExp BTrue state = Right (True :!: state)
-evalExp BFalse state = Right (False :!: state)
-evalExp (Lt intE1 intE2) state =  handleBinExpr intE1 intE2 state (\a b -> a < b)
-evalExp (Gt intE1 intE2) state =  handleBinExpr intE1 intE2 state (\a b -> a > b)
+evalExp (Div intE1 intE2)   state = case evalExp intE1 state of
+                                    Left error -> Left error
+                                    Right (i1 :!: state1) -> case evalExp intE2 state1 of
+                                                             Left error -> Left error
+                                                             Right (0 :!: state2) -> Left DivByZero
+                                                             Right (i2 :!: state2) -> Right ((div i1 i2) :!: state2)
+evalExp BTrue               state = Right (True :!: state)
+evalExp BFalse              state = Right (False :!: state)
+evalExp (Lt intE1 intE2)    state = handleBinExpr intE1 intE2 state (\a b -> a < b)
+evalExp (Gt intE1 intE2)    state = handleBinExpr intE1 intE2 state (\a b -> a > b)
 evalExp (And boolE1 boolE2) state = handleBinExpr boolE1 boolE2 state (\a b -> a && b)
-evalExp (Or boolE1 boolE2) state = handleBinExpr boolE1 boolE2 state (\a b -> a || b)
-evalExp (Not boolE) state = handleUnExpr (evalExp boolE state) (\a -> not a)
-evalExp (Eq intE1 intE2) state = handleBinExpr intE1 intE2 state (\a b -> a == b)
-evalExp (NEq intE1 intE2) state = handleBinExpr intE1 intE2 state (\a b -> a /= b)
+evalExp (Or boolE1 boolE2)  state = handleBinExpr boolE1 boolE2 state (\a b -> a || b)
+evalExp (Not boolE)         state = handleUnExpr (evalExp boolE state) (\a -> not a)
+evalExp (Eq intE1 intE2)    state = handleBinExpr intE1 intE2 state (\a b -> a == b)
+evalExp (NEq intE1 intE2)   state = handleBinExpr intE1 intE2 state (\a b -> a /= b)
